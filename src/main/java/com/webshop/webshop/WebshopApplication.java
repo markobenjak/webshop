ac5d069ca51751c1df4a5b04ecc9c890798f47c5
marko <mbenjak@tvz.hr>
2020-05-26 20:23:07 +0200

    Fixed comments

--- src/main/java/com/webshop/webshop/WebshopApplication.java
+++ src/main/java/com/webshop/webshop/WebshopApplication.java
@@ -14,27 +14,5 @@ public class WebshopApplication {
 
 	public static void main(String[] args) throws SchedulerException {
 		SpringApplication.run(WebshopApplication.class, args);
-	/*	SchedulerFactory schedulerFactory = new StdSchedulerFactory();
-		try {
-			Scheduler scheduler = schedulerFactory.getScheduler();
-
-			JobDetail job = JobBuilder.newJob(SimpleJob.class)
-					.withIdentity("myJob", "group1")
-					.usingJobData("jobSays", "Hello World!")
-					.usingJobData("myFloatValue", 3.141f)
-					.build();
-
-			Trigger trigger = TriggerBuilder.newTrigger()
-					.withIdentity("myTrigger", "group1")
-					.startNow().withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(10).repeatForever())
-					.build();
-
-			scheduler.scheduleJob(job, trigger);
-			scheduler.start();
-
-		} catch (SchedulerException e) {
-			e.printStackTrace();
-		}
-	*/
 	}
 }
